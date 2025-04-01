00283f043677aa7427bc517162f5e91e0690efae
Jacobo <yosoyjacobo23@gmail.com>
2025-04-01 14:10:48 -0700

    create folder models

---
+++ bloglist-backend/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const Blog = require('./models/blog')
+
+require('dotenv').config()
+
+mongoose.connect(process.env.MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
