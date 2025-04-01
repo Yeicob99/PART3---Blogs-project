11788cca810e7d9e819515aaae672658d6d841f2
Jacobo <yosoyjacobo23@gmail.com>
2025-04-01 14:28:46 -0700

    created controllers folder with the router

--- bloglist-backend/index.js
+++ bloglist-backend/index.js
@@ -4,6 +4,8 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
+app.use('/api/blogs', blogsRouter)
 
 require('dotenv').config()
 
@@ -11,24 +13,6 @@ mongoose.connect(process.env.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 const PORT = 3003
 app.listen(PORT, () => {
